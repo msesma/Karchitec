b6e38bd2d511b878e1560d570c9d18257ad3412d
Miguel Sesma <miguel@sesma.eu>
2017-06-26 16:26:18 +0200

    Avoid reporting previous  network state after activity recreation

--- app/src/main/java/com/paradigmadigital/karchitect/repository/ErrorLiveData.kt
+++ app/src/main/java/com/paradigmadigital/karchitect/repository/ErrorLiveData.kt
@@ -1,17 +1,32 @@
 package com.paradigmadigital.karchitect.repository
 
+import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
+
 
 class ErrorLiveData
 @Inject
 constructor(
         val executor: MainThreadExecutor
 ) : LiveData<NetworkError>() {
+
+    private val pending = AtomicBoolean(false)
+
     fun setNetworkError(value: NetworkError) {
         executor.execute {
+            pending.set(true)
             super.setValue(value)
         }
     }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<NetworkError>) =
+            super.observe(owner, Observer<NetworkError> { value ->
+                if (pending.compareAndSet(true, false)) {
+                    observer.onChanged(value)
+                }
+            })
 }
 
